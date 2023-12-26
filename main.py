473fa3e553abe49f59795d648fba99e245e8daf2
kranpant <110833432+kranpant@users.noreply.github.com>
2023-12-26 18:43:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @𝐏𝐲𝐗𝐞𝐧")
+print("\n@TEAM_SMOKER BOT DEPLOYED SUCCESSFULLY ⚡\nMy Master ---> @TEAM_SMOKER")
 
 
 async def main():
